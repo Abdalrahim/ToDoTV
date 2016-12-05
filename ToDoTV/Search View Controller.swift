931270c8ab109289828631aed79eb5f16a03c248
Abdalrahim Abdulla <abdalrahim.abdullah1@gmail.com>
2016-12-05 19:25:25 +0300

    Search working

--- ToDoTV/Search View Controller.swift
+++ ToDoTV/Search View Controller.swift
@@ -12,7 +12,7 @@ import Alamofire
 import AlamofireImage
 import AlamofireNetworkActivityIndicator
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     @IBOutlet weak var searchTV: UISearchBar!
     
@@ -24,7 +24,11 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         super.viewDidLoad()
         resultTable.delegate = self
         resultTable.dataSource = self
-        API()
+        
+        searchTV.delegate = self
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -33,16 +37,23 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        resultTable.reloadData()
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        API()
     }
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        dismissKeyboard()
+    }
+    
+    
     func API() {
-        let urlString = "http://api.tvmaze.com/search/shows?q=gotham"
+        let urlString = "http://api.tvmaze.com/search/shows?q=\(searchTV.text!)"
         Alamofire.request(urlString, method: .get, encoding: JSONEncoding.default, headers: [:]).validate().responseJSON() { response in
             switch response.result {
             case .success( _):
                 if let value = response.result.value {
+                    
+                    self.aseries.removeAll()
                     
                     let json = JSON(value)
                     
@@ -50,13 +61,12 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
                     // The rest is all boiler plate code you'll use for API request
                     
                     let jsonMovies = json.arrayValue
-                    //var movieObjectArray: [SeriesSearch] = []
-                    
                     
                     for series in jsonMovies{
                         self.aseries.append(SeriesSearch(json: series))
                     }
                     
+                    self.resultTable.reloadData()
                     
                 }
             case .failure(let error):
@@ -66,6 +76,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
         
     }
+    
     @nonobjc func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -84,18 +95,25 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         let url = URL(string: aseries[indexPath.row].posterImageView)
         
-        DispatchQueue.global().async {
+        if url == nil  {
             
-            let data = try? Data(contentsOf: url!)
-            //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+        }
+        else {
             
-            if data == nil {
+            
+            DispatchQueue.global().async {
                 
-            }
-            else {
+                let data = try? Data(contentsOf: url!)
+                //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
                 
-                DispatchQueue.main.async {
-                    cell.posterImage.image = UIImage(data: data!)
+                if data == nil {
+                    
+                }
+                else {
+                    
+                    DispatchQueue.main.async {
+                        cell.posterImage.image = UIImage(data: data!)
+                    }
                 }
             }
         }
@@ -104,4 +122,13 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     
+}
+extension SearchViewController {
+    
+    //simple dismissing the keyboard
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+        
+    }
 }
